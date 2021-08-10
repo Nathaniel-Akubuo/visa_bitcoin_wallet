a0504b83399870d6a5e74a934d520e6b349cd449
Nathaniel Akubuo <nakubuo@gmail.com>
2021-08-10 15:24:36 +0100

    latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         navigatorKey: StackedService.navigatorKey,
         onGenerateRoute: StackedRouter().onGenerateRoute,
-        initialRoute: Routes.homeInitial,
+        initialRoute: Routes.signUp,
       ),
     );
   }
